6fb805de5cfcd662a6f494247b6d2872ca2aeb7c
arif afzal <arif.afzal_cs20@gla.ac.in>
2021-12-06 22:02:27 +0530

    Return type

--- src/Return_type.java
+++ src/Return_type.java
@@ -1,6 +1,6 @@
 public class Return_type {
 
-   protected void show()
+    void show() throws RuntimeException
     {
         System.out.println("5");
 
@@ -8,7 +8,7 @@ public class Return_type {
 }
 class boy extends Return_type
 {
-    public void show()
+     void show() throws ArithmeticException
     {
         System.out.println("8");
     }
