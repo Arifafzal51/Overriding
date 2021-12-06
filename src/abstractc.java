6290cae238f0151b1b0f414de4106b2bbbc26bed
arif afzal <arif.afzal_cs20@gla.ac.in>
2021-12-07 00:05:54 +0530

    Abstract

--- src/abstractc.java
+++ src/abstractc.java
@@ -12,6 +12,7 @@ abstract class Bank{
         public static void main(String[] args) {
             Bank b;
             b = new SBI();
+
             System.out.println("Rate of Interest is: " + b.getRateOfInterest() + " %");
             b = new PNB();
             System.out.println("Rate of Interest is: " + b.getRateOfInterest() + " %");
